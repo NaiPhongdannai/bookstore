eef452c405e8b6b4bec4e245867cc4db3d194d2a
NaiPhongdannai <96970022+NaiPhongdannai@users.noreply.github.com>
2026-07-23 20:39:44 +0700

    update test

--- db.js
+++ db.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 // Create connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  host: process.env.DB_HOST || 'mysql-1b83004f-book-1.f.aivencloud.com',
+  user: process.env.DB_USER || 'avnadmin',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bookstore_db',
+  database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
