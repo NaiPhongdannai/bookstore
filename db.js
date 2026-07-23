045759de92cd341d34071b5a6314ebcd9c0544d1
NaiPhongdannai <96970022+NaiPhongdannai@users.noreply.github.com>
2026-07-23 20:43:25 +0700

    Update db.js

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'mysql-1b83004f-book-1.f.aivencloud.com',
   user: process.env.DB_USER || 'avnadmin',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'AVNS_9NBnxqCeQhXTjr4i941',
   database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
